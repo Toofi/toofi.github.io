e2b3b544b0600dcae882341aaf371caf7f715900
Toofi <herpoel.quentin@gmail.com>
2024-07-09 22:08:01 +0200

    added profile + first styling &  first components, first landing page skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SocialsComponent } from "./components/socials/socials.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { ProfileComponent } from './components/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    SocialsComponent, 
+    MenuComponent, 
+    ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'toofi.github.io';
-  imagePath: string = "assets/profile.jpg";
 }
