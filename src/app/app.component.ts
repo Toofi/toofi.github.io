ed44fbf62826cf3fc2ce120ef87003960e5eb0e9
Toofi <herpoel.quentin@gmail.com>
2024-08-01 15:09:12 +0200

    added routing & about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { SocialsComponent } from "./components/socials/socials.component";
 import { MenuComponent } from "./components/menu/menu.component";
 import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from "./pages/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, 
-    SocialsComponent, 
-    MenuComponent, 
-    ProfileComponent],
+  imports: [RouterOutlet,
+    SocialsComponent,
+    MenuComponent,
+    ProfileComponent, 
+    AboutComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent,
+    title: 'Landing Page'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About Me'
+  }
+];
